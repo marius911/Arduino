2434c7639c72c77b72dc02d150ebe5b42e478535
Marius Ancutescu <marius.ancutescu1@gmail.com>
2018-01-06 21:58:51 +0200

    Adaugat ethernet

--- CoolIT_alpha_v1.ino
+++ CoolIT_alpha_v1.ino
@@ -2,11 +2,38 @@
 #include <Wire.h> 
 #include <LCD.h>
 #include <LiquidCrystal_I2C.h>
+#include <SPI.h>
+#include <Ethernet.h>
 
-//Variabile
-float hum;  //Stores humidity value 
-float temp; //Stores temperature value
+
+//Variabile senzor
 Adafruit_HTU21DF htu = Adafruit_HTU21DF(); // pentru senzor
+float hum ;  //Stores humidity value 
+float temp ; //Stores temperature value
+
+
+//setare ethernet
+byte mac[] = { 0x3F1E8F397838}; //Setting MAC Address
+char server[] = "api.pushingbox.com"; //pushingbox API server
+IPAddress ip(192,168,1,2); //Arduino IP address. Only used when DHCP is turned off.
+EthernetClient client; //define 'client' as object
+String data; //GET query with data
+boolean conexi = false;
+
+// functii pt trimis datele
+void IaData(){
+  data+="";
+  data+="GET /pushingbox?devid=v257BB0252CCEE9F&tempData="; //GET request query to pushingbox API
+  data+=temp;
+  data+=" HTTP/1.1";
+}
+void sendData(){
+  Serial.println("connected");
+  client.println(data);
+  client.println("Host: api.pushingbox.com");
+  client.println("Connection: close");
+  client.println();}
+
 
 //Setare display
 #define I2C_ADDR 0x3F
@@ -36,12 +63,11 @@ void temp_umid_pe_display()
 
   //setare buzzer
  const int pin_de_buzzer = 12; //buzzer to arduino pin 12 
-  void buzzer()
-  {tone(pin_de_buzzer, 1000, 500);} // Send 1KHz sound signal... 
+  void buzzer() {tone(pin_de_buzzer, 1000, 500);} // Send 1KHz sound signal... 
 
 
-void setup()
-{
+void setup() {
+
   Serial.begin(9600);
  
   lcd.begin(16,2);
@@ -52,15 +78,20 @@ void setup()
 
   // Set buzzer - pin 12 as an output
   pinMode(pin_de_buzzer, OUTPUT); 
+  
+  //Setare Ethernet
+  if (Ethernet.begin(mac) == 0) {
+  Serial.println("Failed to configure Ethernet using DHCP");
+  Ethernet.begin(mac, ip);
+  }
+
 }
 
 void loop()
 {
-  
-  delay(2000);
-  //Read data and store it to variables hum and temp
   hum = htu.readHumidity();
   temp = htu.readTemperature();
+  
   
   //Print temp and humidity values to serial monitor
   Serial.print("Humidity: ");
@@ -76,5 +107,35 @@ temp_umid_pe_display();
   if(temp > 25 || temp < 5)
   {buzzer();}
 
+ 
+   IaData(); //packing GET query with data
+   Serial.println("connecting...");
+   if (client.connect(server, 80)) {
+     sendData();  
+     conexi = true; //connected = true
+   }
+   else{
+     Serial.println("connection failed");
+   }
+  // loop
+  while(conexi){
+    if (client.available()) {
+    char c = client.read(); //save http header to c
+    Serial.print(c); //print http header to serial monitor
+    }
+    if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting.");
+          Serial.print("Temperature Sent :");
+          Serial.println(temp); //print sent value to serial monitor
+    client.stop(); 
+          conexi = false; 
+          data = ""; //data reset
+    }
+}
+delay(10000);
 
 }
+
+
+
